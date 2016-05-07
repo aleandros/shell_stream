4ebebede6782050987676e20b76787d0839b894a
Edgar Cabrera <edgar@cafeinacode.com>
2016-05-07 00:17:01 -0500

    Update mix.exs for publishing

--- mix.exs
+++ mix.exs
@@ -7,27 +7,36 @@ defmodule ShellStream.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
+     description: description,
      deps: deps]
   end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_doc, "~> 0.11", only: :dev},
      {:earmark, "~> 0.1", only: :dev}]
+  end
+
+  defp description do
+    """
+    Library that exposes a single function (and sigil)
+    for running shell commands and returning a stream
+    which generates an element for every line of
+    the command output.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Edgar Cabrera"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/aleandros/shell_stream",
+	       "Docs" => "http://hexdocs.pm/shell_stream/"}
+    ]
   end
 end
