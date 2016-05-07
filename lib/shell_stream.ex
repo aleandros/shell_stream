b459870d1b4c66d022726f20ade27146d8cf6b2d
Edgar Cabrera <edgar@cafeinacode.com>
2016-05-06 23:51:09 -0500

    Implement the shell stream module

--- lib/shell_stream.ex
+++ lib/shell_stream.ex
@@ -1,2 +1,46 @@
 defmodule ShellStream do
+  @moduledoc """
+  Simple interface for dealing with shell commands
+  as streams.
+  
+  Exposes the functionality via the `shell` function
+  or with `sigil_x`. Probably useful for using
+  elixir in shell scripts.
+  """
+  alias ShellStream.PortHandler
+
+  @doc """
+  Exposes a shell command as an elixir stream.
+  As simple as that.
+  """
+  # TODO find out how to specify Stream as
+  # return type in a spec
+  def shell(command) do
+    Stream.resource(
+      fn ->
+	spawn(PortHandler, :run, [command, self])
+	self
+      end,
+      fn owner ->
+	receive do
+	  {:line, line} ->
+	    {[line], owner}
+	  {:status, _status} ->
+	    {:halt, owner}
+	end
+      end,
+      fn _ -> nil end
+    )
+  end
+
+  @doc """
+  Wraps the shell command around
+  the ~x sigil for nicer syntax, but
+  it is completely optional.
+  
+  Currently supports no options.
+  """
+  def sigil_x(command, []) do
+    shell(command)
+  end
 end
--- test/shell_stream_test.exs
+++ test/shell_stream_test.exs
@@ -2,7 +2,26 @@ defmodule ShellStreamTest do
   use ExUnit.Case
   doctest ShellStream
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "shell works with a simple command" do
+    output = ShellStream.shell("echo 'hello world'")
+    |> Enum.to_list
+    assert output == ["hello world"]
+  end
+
+  test "shell can be chained with stream functions" do
+    output = ShellStream.shell("seq 10")
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.filter(&(rem(&1, 2) == 0))
+    |> Enum.to_list
+    assert output == [2, 4, 6, 8, 10]
+  end
+
+  test "sigil syntax works the same way" do
+    import ShellStream
+    output = ~x(seq 10)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.filter(&(rem(&1, 2) == 0))
+    |> Enum.to_list
+    assert output == [2, 4, 6, 8, 10]
   end
 end
